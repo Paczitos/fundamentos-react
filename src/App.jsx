58a521916dac7cb49992257125123fe454471072
techfim <arturamaral97@gmail.com>
2025-05-06 18:56:28 -0300

    main limpa e corrigida

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './global.css'
-import { Post } from './Post'
+import { Post } from './components/Post'
 import styles from './App.module.css'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
