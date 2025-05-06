d9ba1bb66b0472ef7c4fd37930ac78e658ace1a4
techfim <arturamaral97@gmail.com>
2025-05-06 11:09:25 -0300

    Sidebar component

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './global.css'
+import { Post } from './Post'
+import styles from './App.module.css'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar></Sidebar>
+
+        <main>
+          <Post
+            author="Artur Amaral"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam porro quia ducimus cumque soluta expedita eum corporis minus consectetur? Excepturi inventore veritatis praesentium necessitatibus fuga iusto recusandae quae assumenda."
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
