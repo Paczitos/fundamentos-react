e1839218b8271f1f73067a3c3e01307936826477
techfim <arturamaral97@gmail.com>
2025-05-06 18:32:41 -0300

    recuperando a main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { Header } from './components/Header'
-import { Post } from './Post'
-
 import './global.css'
+import { Post } from './Post'
+import styles from './App.module.css'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post
-        author="Artur Amaral"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam porro quia ducimus cumque soluta expedita eum corporis minus consectetur? Excepturi inventore veritatis praesentium necessitatibus fuga iusto recusandae quae assumenda."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar></Sidebar>
+
+        <main>
+          <Post
+            author="Artur Amaral"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam porro quia ducimus cumque soluta expedita eum corporis minus consectetur? Excepturi inventore veritatis praesentium necessitatibus fuga iusto recusandae quae assumenda."
+          />
+        </main>
+      </div>
     </div>
   )
 }
